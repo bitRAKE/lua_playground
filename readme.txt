53296d40ad73626edc02b5cd4737dc9c25750a30
Rickey Bowers Jr <bitrake@gmail.com>
2021-02-04 13:59:57 -0700

    first view of the tests on windows 10 build

--- readme.txt
+++ readme.txt
@@ -1,3 +1,10 @@
 References:
 
+
+look at interesting changing happening:
+https://github.com/lua/lua/network
+
+
+introduction of lua use through C
 https://github.com/tylerneylon/APIsWithLua
+
--- win_lua/script.c
+++ win_lua/script.c
@@ -1,13 +1,19 @@
-#include <filesystem>
-
 #include "lauxlib.h"
 #include "lua.h"
 #include "lualib.h"
 
-int main() {
-	_chdir("C:/Y/lua/lua_benchmarking");
+int main(int argc, char* argv[]) {
+	chdir("../../../../lua/testes");
 	lua_State* L = luaL_newstate();
 	luaL_openlibs(L);
-	luaL_dofile(L, "simplerunner.lua");
+
+	printf("argc = %d\n", argc);
+	for (int ndx = 0; ndx != argc; ++ndx) {
+		printf("argv[%d] --> %s\n", ndx, argv[ndx]);
+		if (ndx > 0) {
+			luaL_dofile(L, argv[ndx]);
+		}
+	}
+
 	return 0;
 }
