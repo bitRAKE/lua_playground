ed0c8781e38fcad1c63dc8a9531f1e1a935bca81
sit-d <281093367+sit-d@users.noreply.github.com>
2026-07-05 00:55:14 -0600

    fix: run each Lua test wrapper argument

--- win_lua/test.cmd
+++ win_lua/test.cmd
@@ -1,1 +1,8 @@
-for %%G in (../lua/testes/%*) do script %%G
+@echo off
+if "%~1"=="" (
+    echo Usage: test ^<lua-test-name^> [more-test-names...]
+    echo Example: test bitwise.lua calls.lua
+    exit /b 1
+)
+
+for %%G in (%*) do script "..\lua\testes\%%~G"
